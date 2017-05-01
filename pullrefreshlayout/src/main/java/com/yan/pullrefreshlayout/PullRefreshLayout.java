9b253009d00e7444c6768b8dbf0a2e7a6b0f247c
yan <yanxianwei1114@gmail.com>
2017-05-01 16:40:15 +0800

    refreshLayout

--- pullrefreshlayout/src/main/java/com/yan/pullrefreshlayout/PullRefreshLayout.java
+++ pullrefreshlayout/src/main/java/com/yan/pullrefreshlayout/PullRefreshLayout.java
@@ -7,7 +7,6 @@ import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.NestedScrollingParentHelper;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
