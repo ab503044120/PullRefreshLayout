8f2da3186bea988a688064d8e1ad585c0e8795fa
yan <yxw_developer@outlook.com>
2017-05-02 13:42:07 +0800

    动画执行速度调整

--- app/src/main/java/com/yan/refreshloadlayouttest/MainActivity.java
+++ app/src/main/java/com/yan/refreshloadlayouttest/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         refreshLayout = (PullRefreshLayout) findViewById(R.id.refreshLayout);
         refreshLayout.setLoadMoreEnable(true);
 //        refreshLayout.setDuringAdjustValue(10f);// 动画执行时间调节，越大动画执行越慢
-        // 刷新或加载完成后回复动画执行时间，为-1时，根据setDuringAdjustValue（0.4f）方法实现
+        // 刷新或加载完成后回复动画执行时间，为-1时，根据setDuringAdjustValue（）方法实现
 //        refreshLayout.setRefreshBackTime(300);
 //        refreshLayout.setPullViewHeight(100);// 设置头部和底部的高度
 //        refreshLayout.setDragDampingRatio(0.6f);// 阻尼系数
