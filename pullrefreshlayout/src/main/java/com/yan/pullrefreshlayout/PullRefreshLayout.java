87cdd1ebf6a52016a3f8d1051bcd81f4668bcb6a
yan <yxw_developer@outlook.com>
2017-06-07 18:45:34 +0800

    header 和 footer 的高度 来自于 自身的布局文件

--- pullrefreshlayout/src/main/java/com/yan/pullrefreshlayout/PullRefreshLayout.java
+++ pullrefreshlayout/src/main/java/com/yan/pullrefreshlayout/PullRefreshLayout.java
@@ -9,7 +9,6 @@ import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ScrollerCompat;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
