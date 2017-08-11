b6417b096b5a98ed367da5b240911c11fde81f76
yan <yxw_developer@outlook.com>
2017-08-11 14:23:37 +0800

    version:1.6.4 ： 稳定版(Stable version)

--- pullrefreshlayout/src/main/java/com/yan/pullrefreshlayout/GeneralPullHelper.java
+++ pullrefreshlayout/src/main/java/com/yan/pullrefreshlayout/GeneralPullHelper.java
@@ -2,7 +2,6 @@ package com.yan.pullrefreshlayout;
 
 import android.content.Context;
 import android.support.v4.view.MotionEventCompat;
-import android.support.v4.widget.NestedScrollView;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.ViewConfiguration;
